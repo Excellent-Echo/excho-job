6a569f6cb743a06e8230c87058898f330bfa715d
efzedel16 <efzedel16@outlook.com>
2021-06-02 00:16:53 +0700

    Sign-up & sign-in pages are finished in style and responsive

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-import SignUp from "./pages/jobseeker/SignUp";
+// import SignUp from "./pages/SignUp";
+// import SignIn from "./pages/SignIn";
+// import SignIn from "./pages/SignIn";
+// import HeaderMainGuest from "./components/HeaderMainGuest";
+// import InputFormHires from "./pages/hires/InputFormHires";
+// import InputFormHires from "./pages/hires/InputFormHires";
+
+import InputFormJobseeker from "./pages/jobseeker/InputFormJobseeker";
 
 function App() {
   return (
     <>
-      <SignUp />
+      <InputFormJobseeker />
     </>
   );
 }
