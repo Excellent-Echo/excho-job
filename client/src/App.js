df23af66936d98bc49771bc4abf0b2bcc862d717
efzedel16 <efzedel16@outlook.com>
2021-06-03 14:06:11 +0700

    Resolve conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,3 @@
-import RecruiterForm from "./pages/recruiter/RecruiterForm";
-import RecruiterSignUp from "./pages/recruiter/RecruiterSignUp";
-import GuestHeader from "./components/GuestHeader";
-import JobSeekerSignUp from "./pages/jobseeker/JobSeekerSignUp";
-import SignIn from "./pages/SignIn";
-import GuestMainPage from "./pages/GuestMainPage";
-import UserHeader from "./components/UserHeader";
-import JobSeekerForm from "./pages/jobseeker/JobSeekerForm";
 import JobDetails from "./pages/jobseeker/JobDetails";
 
 function App() {
