9bb253aa86b11354a55b4ddc61197ee259a00cbc
efzedel16 <efzedel16@outlook.com>
2021-06-04 00:38:37 +0700

    ExchoJobSlogan clean code

--- client/src/components/ExchoJobSlogan.jsx
+++ client/src/components/ExchoJobSlogan.jsx
@@ -1,8 +1,8 @@
 export default function ExchoJobSlogan() {
   return (
-    <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+    <div className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
       <span className="block xl:inline">Get the dream</span>{" "}
       <span className="block text-indigo-600 xl:inline">job you want</span>
-    </h1>
+    </div>
   );
 }
