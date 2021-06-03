44a8d1e2e7c6ba6cef3422f66434eb6c91d4b67f
mohammedazez <muhamadaziz047@gmail.com>
2021-06-03 13:12:39 +0700

    Job seeker registration

--- client/src/APIs/jobSeekerClient.js
+++ client/src/APIs/jobSeekerClient.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
-const jobSeekerClient = Axios.create({
-    baseURL: "https://excho-job.herokuapp.com/",
+const exchoJobClient = Axios.create({
+  baseURL: "https://excho-job.herokuapp.com/",
 });
 
-export default jobSeekerClient;
+export default exchoJobClient;
--- client/src/redux/user/register/userRegisterReducer.js
+++ client/src/redux/user/register/userRegisterReducer.js
@@ -1,64 +1,66 @@
 const initialState = {
-    fullname: "",
-    email: "",
-    position: "",
-    password: "",
-    errorMessage: "",
-    successMessage: "",
-    isLoading: false,
-}
-
-const userRegisterReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "USER_REGISTER_RESET_FORM":
-            return {
-                ...initialState,
-            };
-        case "USER_REGISTER_SET_FULLNAME":
-            return {
-                ...state, fullname: action.payload.fullname,
-            };
-        case "USER_REGISTER_SET_EMAIL":
-            return {
-                ...state,
-                email: action.payload.email,
-            };
-        case "USER_REGISTER_SET_POSITION":
-            return {
-                ...state, position: action.payload.position,
-            };
-        case "USER_REGISTER_SET_PASSWORD":
-            return {
-                ...state,
-                password: action.payload.password,
-            };
-        case "USER_REGISTER_SET_ERROR_MESSAGE":
-            return {
-                ...state,
-                errorMessage: action.payload.errorMessage,
-            };
-
-        case "USER_REGISTER_SET_SUCCESS_MESSAGE":
-            return {
-                ...state,
-                successMessage: action.payload.successMessage,
-            };
-
-        case "USER_REGISTER_START_LOADING":
-            return {
-                ...state,
-                isLoading: true,
-            };
-
-        case "USER_REGISTER_STOP_LOADING":
-            return {
-                ...state,
-                isLoading: false,
-            };
-
-        default:
-            return state;
-    }
+  fullname: "",
+  email: "",
+  position: "",
+  password: "",
+  errorMessage: "",
+  successMessage: "",
+  isLoading: false,
 };
 
-export default userRegisterReducer
+const userRegisterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "USER_REGISTER_RESET_FORM":
+      return {
+        ...initialState,
+      };
+    case "USER_REGISTER_SET_FULLNAME":
+      return {
+        ...state,
+        fullName: action.payload.fullname,
+      };
+    case "USER_REGISTER_SET_EMAIL":
+      return {
+        ...state,
+        email: action.payload.email,
+      };
+    case "USER_REGISTER_SET_POSITION":
+      return {
+        ...state,
+        position: action.payload.position,
+      };
+    case "USER_REGISTER_SET_PASSWORD":
+      return {
+        ...state,
+        password: action.payload.password,
+      };
+    case "USER_REGISTER_SET_ERROR_MESSAGE":
+      return {
+        ...state,
+        errorMessage: action.payload.errorMessage,
+      };
+
+    case "USER_REGISTER_SET_SUCCESS_MESSAGE":
+      return {
+        ...state,
+        successMessage: action.payload.successMessage,
+      };
+
+    case "USER_REGISTER_START_LOADING":
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case "USER_REGISTER_STOP_LOADING":
+      return {
+        ...state,
+        isLoading: false,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default userRegisterReducer;
