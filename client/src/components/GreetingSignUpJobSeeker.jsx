1758a3fe37354c03122ecb40e4dee505add6051d
efzedel16 <efzedel16@outlook.com>
2021-06-04 00:36:47 +0700

    GreetingSignUpJobSeeker clean code

---
+++ client/src/components/GreetingSignUpJobSeeker.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function GreetingSignUpJobSeeker() {
+  return (
+    <div className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+      Sign up to be Job Seeker
+    </div>
+  );
+}
