64e95be6ac2744bff0059fef3b4fcccc2118028c
efzedel16 <efzedel16@outlook.com>
2021-06-04 00:38:04 +0700

    GreetingSignIn clean code

---
+++ client/src/components/GreetingSignIn.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function GreetingSignIn() {
+  return (
+    <div className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+      Sign in to your account
+    </div>
+  );
+}
