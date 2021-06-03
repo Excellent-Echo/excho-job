1357f795796ce7c4d5a99f27be5a364168348a4e
efzedel16 <efzedel16@outlook.com>
2021-06-04 00:40:52 +0700

    CloseButtonPopover clean code

---
+++ client/src/components/CloseButtonPopover.jsx
@@ -0,0 +1,13 @@
+import { Popover } from "@headlessui/react";
+import { XIcon } from "@heroicons/react/outline";
+
+export default function CloseButtonPopover() {
+  return (
+    <div className="-mr-2">
+      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+        <span className="sr-only">Close menu</span>
+        <XIcon className="h-6 w-6" aria-hidden="true" />
+      </Popover.Button>
+    </div>
+  );
+}
